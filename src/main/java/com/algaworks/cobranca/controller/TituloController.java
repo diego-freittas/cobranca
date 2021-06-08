3a0fb00017d021174f6084d9f6a30632e0728cfe
Diego Freitas <tpddiego@gmail.com>
2021-06-07 21:13:41 -0300

    2.14 Mantendo os valores no formulario

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,11 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
@@ -29,19 +33,21 @@ public class TituloController {
 		// mv.addObject("todosStatusTitulo",StatusTitulo.values()); Refatoramos criando uma lista. 
 		// todosStatusTitulo nome do objeto que vai ser interado na viwer
 		// StatusTitulo.values() valores.
+		mv.addObject(new Titulo()); // Objeto que ta sendo utilizado no view Cadastro titulo, chamado no form. 
+		// <form class="form-horizontal" method="POST" action="/titulos" th:object="${titulo}">
 		return mv;
 	}
 	
 	//@RequestMapping(value="/titulos", method = RequestMethod.POST)
 	@RequestMapping(method = RequestMethod.POST)
 	//Novo método que retorna alem da View um atributo de mensagem
-	public ModelAndView salvar(Titulo titulo) {
-		
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
+		if(errors.hasErrors()) {
+			return "CadastroTitulo";
+		}
 			titulos.save(titulo);
-			
-			ModelAndView mv = new ModelAndView("CadastroTitulo");
-			mv.addObject("mensagem", "Título salvo com sucesso!");
-			return mv; // pagina que eu quero retornar
+			attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+			return "redirect:/titulos/novo" ; // pagina que eu quero retornar
 		 }
 	
 	@RequestMapping
